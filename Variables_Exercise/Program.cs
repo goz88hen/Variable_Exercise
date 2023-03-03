e08c0ad5414dd63d53cb637c84dac6f2dcb46e81
Henry (Hendra) Gozali <123764602+goz88hen@users.noreply.github.com>
2023-03-03 02:43:01 -0800

    Exercise one is completed.

---
+++ Variables_Exercise/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+
+// keyword(initializer; conditional; increment)
+for (int i = 0; i < 16; i++) 
+{ 
+        Console.WriteLine($"{i++}");   //The scope which is the body of the loop  
+}
+
+
+
+
